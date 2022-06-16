1506639ac1cb0806fdcf9f320aed5dc3d8c7ca80
Heeyoung Yang <dev.heeyoung@gmail.com>
2022-06-16 11:24:22 -0400

    client code refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import AuthService from './service/auth';
 import TweetService from './service/tweet';
+import HttpClient from './network/http';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AuthErrorEventBus } from './context/AuthContext';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
+const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
 const authService = new AuthService();
-const tweetService = new TweetService(baseURL);
+const tweetService = new TweetService(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
